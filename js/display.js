e6285352f9b406de8e29c60d7133c6c36b785d31
JackFrathbone <2282711@students.wits.ac.za>
2021-09-30 09:43:19 +0200

    Added website framework

---
+++ js/display.js
@@ -0,0 +1,45 @@
+const header = document.createElement('header');
+
+const SiteTitle = document.createElement('div');
+SiteTitle.className = "siteTitle";
+SiteTitle.innerText = "Game State SA"
+header.appendChild(SiteTitle);
+
+
+const nav = document.createElement('nav');
+nav.className = "menu";
+header.appendChild(nav);
+
+const menuList = document.createElement('ul');
+
+
+const MenuItems = [
+    { title: 'Home', link: './Index.html' },
+    { title: 'Blogs', link: './Blogs.html' },
+    { title: 'About', link: './About.html' },
+    { title: 'Credits', link: './Credits.html' },
+    { title: 'Contact', link: './Contact.html' }
+];
+
+for (let item of MenuItems) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.innerText = item.title;
+    a.href = item.link;
+    li.appendChild(a);
+    menuList.appendChild(li);
+}
+
+
+
+nav.appendChild(menuList);
+///////////////////////////////////////
+const footer = document.createElement('footer');
+
+const creditText = document.createElement('p');
+creditText.innerText = "@Jack Rathbone 2021";
+footer.append(creditText);
+
+document.body.prepend(header);
+document.body.appendChild(footer);
+
